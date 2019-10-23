6b095a730ee1664104cdc392d5e045c0b6c73eeb
WheelerLee <490386516@qq.com>
2019-10-23 14:21:15 +0800

    1.0.1

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './Base';
+export * from './BaseFormItem';
+export * from './Form';
+export * from './Hidden';
+export * from './Input';
+export * from './Submit';
+export * from './Switch';
