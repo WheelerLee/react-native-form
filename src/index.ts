64fc9ca7cb777cbcd6b7bb2eb07d8d7825dc7566
WheelerLee <490386516@qq.com>
2020-05-13 17:15:08 +0800

    a

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import Form from './Form';
-import {BaseInput, FormItemProps} from './Base';
+import { BaseInput, FormItemProps } from './Base';
 import BaseFormItem from './BaseFormItem';
 import Hidden from './Hidden';
 import Input from './Input';
 import Switch from './Switch';
 import Submit from './Submit';
 
-export {BaseInput, FormItemProps, BaseFormItem, Hidden, Input, Switch, Submit};
+export { BaseInput, FormItemProps, BaseFormItem, Hidden, Input, Switch, Submit };
 export default Form;
